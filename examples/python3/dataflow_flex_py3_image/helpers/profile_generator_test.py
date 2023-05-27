cf29bfe7f02cd41394d7fcf1384bd21e5adee0eb
Patrick Selamy <pselamy@gmail.com>
2023-05-27 12:17:51 -0400

    Update profile_generator_test.py

--- examples/python3/dataflow_flex_py3_image/helpers/profile_generator_test.py
+++ examples/python3/dataflow_flex_py3_image/helpers/profile_generator_test.py
@@ -1,19 +1,31 @@
 import unittest
-from mymodule import generate_profiles
+
+from helpers import profile_generator
 
 
 class GenerateProfilesTestCase(unittest.TestCase):
-    def test_generate_profiles(self):
-        # Test case 1: Generate 5 profiles
-        profiles = list(generate_profiles(5))
+    def test_generate_profiles_count(self):
+        # Test generating 5 profiles
+        profiles = list(profile_generator.generate_profiles(5))
         self.assertEqual(len(profiles), 5)
+
+    def test_generate_profiles_types(self):
+        # Test profile attribute types
+        profiles = list(profile_generator.generate_profiles(5))
         for profile in profiles:
             self.assertIsInstance(profile.name, str)
-            self.assertTrue(18 <= profile.age <= 99)
+            self.assertIsInstance(profile.age, int)
             self.assertIsInstance(profile.email, str)
 
-        # Test case 2: Generate 0 profiles
-        profiles = list(generate_profiles(0))
+    def test_generate_profiles_age_range(self):
+        # Test age range of generated profiles
+        profiles = list(profile_generator.generate_profiles(5))
+        for profile in profiles:
+            self.assertTrue(18 <= profile.age <= 99)
+
+    def test_generate_profiles_zero_count(self):
+        # Test generating 0 profiles
+        profiles = list(profile_generator.generate_profiles(0))
         self.assertEqual(len(profiles), 0)
 
 
