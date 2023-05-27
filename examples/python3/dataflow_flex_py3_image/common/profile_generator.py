d9aaf5e403a3a562e035f6453aa2736735f58903
Patrick Selamy <pselamy@gmail.com>
2023-05-27 12:59:23 -0400

    Update profile_generator.py

--- examples/python3/dataflow_flex_py3_image/common/profile_generator.py
+++ examples/python3/dataflow_flex_py3_image/common/profile_generator.py
@@ -4,17 +4,9 @@ from typing import Iterable
 
 import faker
 
+from common import models
 
-@dataclasses.dataclass(frozen=True)
-class Profile:
-    """Represents a user profile with name, age, and email."""
-
-    name: str
-    age: int
-    email: str
-
-
-def generate_profiles(count: int) -> Iterable[Profile]:
+def generate_profiles(count: int) -> Iterable[models.Profile]:
     """Generate a specified number of random user profiles.
 
     Args:
@@ -29,4 +21,4 @@ def generate_profiles(count: int) -> Iterable[Profile]:
         name = fake.name()
         age = random.randint(18, 99)
         email = fake.email()
-        yield Profile(name, age, email)
+        yield models.Profile(name, age, email)
