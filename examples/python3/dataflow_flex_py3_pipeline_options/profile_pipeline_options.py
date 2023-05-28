6748a6836eb1c0f8940055c74dd8d43f92ae6b7a
Patrick Selamy <pselamy@gmail.com>
2023-05-28 00:28:50 -0400

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- examples/python3/dataflow_flex_py3_pipeline_options/profile_pipeline_options.py
+++ examples/python3/dataflow_flex_py3_pipeline_options/profile_pipeline_options.py
@@ -1,23 +1,24 @@
 from apache_beam.options import pipeline_options
 
+
 class ProfilePipelineOptions(pipeline_options.PipelineOptions):
     @classmethod
     def _add_argparse_args(cls, parser):
         parser.add_argument(
-            '--input',
-            dest='input',
-            default='gs://my-bucket/input',
-            help='Input file or directory for the profile pipeline.'
+            "--input",
+            dest="input",
+            default="gs://my-bucket/input",
+            help="Input file or directory for the profile pipeline.",
         )
         parser.add_argument(
-            '--output',
-            dest='output',
-            default='gs://my-bucket/output',
-            help='Output directory for the profile pipeline.'
+            "--output",
+            dest="output",
+            default="gs://my-bucket/output",
+            help="Output directory for the profile pipeline.",
         )
         parser.add_argument(
-            '--profile_id',
-            dest='profile_id',
-            default='12345',
-            help='ID of the profile for the profile pipeline.'
+            "--profile_id",
+            dest="profile_id",
+            default="12345",
+            help="ID of the profile for the profile pipeline.",
         )
